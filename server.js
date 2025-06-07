ed9b282954125106d31b71586c4e570ae5481446
LAMAMALONA\mario <mariodavidsoler12@gmail.com>
2025-06-07 16:14:21 -0600

    a

--- server.js
+++ server.js
@@ -6,9 +6,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//rut
+app.use('/api/auth', require('./routes/authRoutes'));
 
 // Puerto
 const PORT = process.env.PORT || 3001;
 app.listen(
     PORT, 
-    ()=>console.log(`Servidor corriendo perra en el puerto ${PORT}`));
+    () => console.log(`Servidor corriendo perra en el puerto ${PORT}`)
+);
